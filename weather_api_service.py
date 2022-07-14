cd4babdb2edaa9bba18abd3b0e2c94c546f62122
Вербило Илья Александрович <verbilo@rit.va>
2022-07-14 08:44:46 +0300

    added weather type

--- weather_api_service.py
+++ weather_api_service.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from datetime import datetime
+from enum import Enum
 from typing import TypeAlias
 
 from coordinates import Coordinates
@@ -7,10 +8,20 @@ from coordinates import Coordinates
 Celsius: TypeAlias = int
 
 
+class WeatherType(Enum):
+    THUNDERSTORM = "Гроза"
+    DRIZZLE = "Изморось"
+    RAIN = "Дождь"
+    SNOW = "Снег"
+    CLEAR = "Ясно"
+    FOG = "Туман"
+    CLOUDS = "Облачно"
+
+
 @dataclass(slots=True, frozen=True)
 class Weather:
     temperature: Celsius
-    weather_type: str
+    weather_type: WeatherType
     sunrise: datetime
     sunset: datetime
     city: str
