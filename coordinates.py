a4a98b9cee07bfe10202372c3e5783633d473bb6
Вербило Илья Александрович <verbilo@rit.va>
2022-08-03 18:12:10 +0300

    get coordinates

--- coordinates.py
+++ coordinates.py
@@ -1,12 +1,31 @@
 from dataclasses import dataclass
+from subprocess import Popen, PIPE
+
+from exceptions import CantGetCoordinates
+
+import json
 
 
-@dataclass(slots=True, frozen=True)
+# @dataclass(frozen=True, slots=True)
+@dataclass
 class Coordinates:
+    __slots__ = ('latitude', 'longitude')
     latitude: float
     longitude: float
 
 
 def get_gps_coordinates() -> Coordinates:
     """Returns current coordinates using IP"""
-    return Coordinates(longitude=10, latitude=20)
+    process = Popen(["curl", "https://ipinfo.io/"], stdout=PIPE)
+    (output, err) = process.communicate()
+    exit_code = process.wait()
+    if err is not None or exit_code != 0:
+        raise CantGetCoordinates
+    data = json.loads(output)['loc']
+    latitude, longitude = data.strip().split(',')
+    return Coordinates(longitude=float(longitude), latitude=float(latitude))
+
+
+if __name__ == "__main__":
+    print(get_gps_coordinates())
+
