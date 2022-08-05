67bddcbdcbb083b3ec17273b6b7d7251e27c7a90
Вербило Илья Александрович <verbilo@rit.va>
2022-08-05 17:06:26 +0300

    weather printer

--- weather.py
+++ weather.py
@@ -2,12 +2,20 @@
 from coordinates import get_gps_coordinates
 from weather_api_service import get_weather
 from weather_formatter import format_weather
+from exceptions import CantGetCoordinates, ApiServiceError
 
 
 def main():
-    coordinates = get_gps_coordinates()
-    print(f"Широта:", coordinates.latitude)
-    weather = get_weather(coordinates)
+    try:
+        coordinates = get_gps_coordinates()
+    except CantGetCoordinates:
+        print("Не смог получить GPS-координаты")
+        exit(1)
+    try:
+        weather = get_weather(coordinates)
+    except ApiServiceError:
+        print("Не смог получить погоду в API-сервиса погоды")
+        exit(1)
     print(format_weather(weather))
 
 
