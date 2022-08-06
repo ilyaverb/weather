3eee796e923d50dd8ca1d4c86b57a07e4a685975
Вербило Илья Александрович <verbilo@rit.va>
2022-08-06 08:36:01 +0300

    added json-storage

--- history.py
+++ history.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from pathlib import Path
-from typing import Protocol
+from typing import Protocol, TypedDict, List
+import json
 
 from weather_api_service import Weather
 from weather_formatter import format_weather
@@ -22,6 +23,38 @@ class PlainFileWeatherStorage:
         formatted_weather = format_weather(weather)
         with open(self._file, "a") as f:
             f.write(f"{now}\n{formatted_weather}\n")
+
+
+class HistoryRecord(TypedDict):
+    date: str
+    weather: str
+
+
+class JSONFileWeatherStorage:
+    """Store weather in JSON file"""
+    def __init__(self, jsonfile: Path):
+        self._jsonfile = jsonfile
+        self._init_storage()
+
+    def save(self, weather: Weather) -> None:
+        history = self._read_history()
+        history.append({
+            "date": str(datetime.now()),
+            "weather": format_weather(weather)
+        })
+        self._write(history)
+
+    def _init_storage(self) -> None:
+        if not self._jsonfile.exists():
+            self._jsonfile.write_text("[]")
+
+    def _read_history(self) -> List[HistoryRecord]:
+        with open(self._jsonfile, "r") as f:
+            return json.load(f)
+
+    def _write(self, history: List[HistoryRecord]) -> None:
+        with open(self._jsonfile, "w") as f:
+            json.dump(history, f, ensure_ascii=False, indent=4)
 
 
 def save_weather(weather: Weather, storage: WeatherStorage) -> None:
